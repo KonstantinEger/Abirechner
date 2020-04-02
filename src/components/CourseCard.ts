94528172002b0d705811d89935c56fc02d716179
KonstantinEger <35273325+KonstantinEger@users.noreply.github.com>
2020-04-02 18:22:06 +0200

    display average for a course

--- src/components/CourseCard.ts
+++ src/components/CourseCard.ts
@@ -2,8 +2,17 @@ import { ICourse } from "../services/db";
 import { concatArray, avgArray, preciseRound } from './utils';
 
 function getCourseCardHTML(semester: number, course: ICourse): string {
-  const roundExamsAvg = preciseRound(avgArray(course.exams[semester]), 2);
-  const roundMarksAvg = preciseRound(avgArray(course.marks[semester]), 2);
+  const examsAvg = avgArray(course.exams[semester]);
+  const marksAvg = avgArray(course.marks[semester]);
+  const courseAvg = isNaN(examsAvg) 
+    ? marksAvg 
+    : isNaN(marksAvg)
+      ? examsAvg
+      : (examsAvg + marksAvg) * 0.5;
+
+  const roundExamsAvg = preciseRound(examsAvg, 2);
+  const roundMarksAvg = preciseRound(marksAvg, 2);
+  const roundCourseAvg = preciseRound(courseAvg, 2);
 
   return `
     <div
@@ -16,13 +25,16 @@ function getCourseCardHTML(semester: number, course: ICourse): string {
         <button id="${course.short_name}-${semester}" onclick="handleAddGrade(event)">
           + Neue Note
         </button>
+        <span class="course-avg">&oslash;${isNaN(roundCourseAvg) ? '-' : roundCourseAvg}</span>
       </div>
+
       <div class="course-card-body">
         <b>Klausuren</b>
         <span class="grades-avg">&oslash;${isNaN(roundExamsAvg) ? '-' : roundExamsAvg}</span>
         <br />
           ${concatArray(course.exams[semester])}
         <br />
+
         <b>Leistungskontrollen</b>
         <span class="grades-avg">&oslash;${isNaN(roundMarksAvg) ? '-' : roundMarksAvg}</span>
         <br />

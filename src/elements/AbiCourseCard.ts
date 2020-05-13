3beb7d8571ede36fb40da98b48d5084c800f9a82
KonstantinEger <35273325+KonstantinEger@users.noreply.github.com>
2020-05-13 13:16:12 +0200

    card styling and interpolation

--- src/elements/AbiCourseCard.ts
+++ src/elements/AbiCourseCard.ts
@@ -2,14 +2,50 @@ import { Course } from "../db";
 
 const template = document.createElement('template');
 template.innerHTML = `
-  <h1>Hello World</h1>
-  <p><span id="course-name"></span> semester <span id="sem"></span></p>
+  <style>
+    .course-card {
+      padding: 10px;
+      margin: 15px;
+      border-radius: 5px;
+      background-color: var(--bg-color-lighten);
+      border-left: 3px solid var(--bg-color-lighten);
+    }
+
+    .course-card-header {
+      padding: 10px 0px 10px 10px;
+      font-size: 1.2em;
+    }
+
+    .course-card-body {
+      padding-left: 13px;
+    }
+  </style>
+  <div class="course-card" style="border-color:{{ COLOR }}">
+    <div class="course-card-header">
+      {{ NAME }}
+    </div>
+
+    <div class="course-card-body">
+      <b>Klausuren</b> <br />
+      {{ EXAMS }}
+      <br />
+      <b>Leistungskontrollen</b> <br />
+      {{ MARKS }}
+      <br />
+    </div>
+  </div>
 `;
 
 export class AbiCourseCardElement extends HTMLElement {
   public static readonly selector = 'abi-course-card';
+  public shadowRoot: ShadowRoot;
   private course: Course | null = null;
   private semester: number | null = null;
+
+  constructor() {
+    super();
+    this.shadowRoot = this.attachShadow({ mode: 'open' });
+  }
 
   public static defineIfNot(): void {
     if (window.customElements.get(AbiCourseCardElement.selector) === undefined) {
@@ -18,16 +54,27 @@ export class AbiCourseCardElement extends HTMLElement {
   }
 
   public connectedCallback(): void {
-    this.attachShadow({ mode: 'open' });
-
     if (!this.course || this.semester === null) throw new Error();
-    this.shadowRoot?.appendChild(template.content.cloneNode(true));
-    this.shadowRoot?.getElementById('course-name')!.innerText = this.course.name;
-    this.shadowRoot?.getElementById('sem')!.innerText = this.semester + 1 + '';
+
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    this.replaceTemp(
+      [/{{ COLOR }}/, this.course.color],
+      [/{{ NAME }}/, this.course.name],
+      [/{{ EXAMS }}/, this.course.exams[this.semester].toString()],
+      [/{{ MARKS }}/, this.course.marks[this.semester].toString()]
+    );
   }
 
   public setCourseAndSem(course: Course, sem: number): void {
     this.course = course;
     this.semester = sem;
+  }
+
+  private replaceTemp(...values: [string | RegExp, string][]): void {
+    let innerHTML = this.shadowRoot.innerHTML;
+    for (const arg of values) {
+      innerHTML = innerHTML.replace(arg[0], arg[1]);
+    }
+    this.shadowRoot.innerHTML = innerHTML;
   }
 }
